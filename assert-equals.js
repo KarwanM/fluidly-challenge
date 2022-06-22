fe2e06e6a18a79c72c24da9efc61304002991a45
karwanm <karwan.ismo@gmail.com>
2022-06-22 17:05:33 +0100

    add assertNotEquals function and test the challenges

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,62 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+  // If expect and actual are equal
+  if (expect === actual) {
+    console.log("_No Error_");
+  }
+}
 
-module.exports = assertEquals
+function assertNotEquals(expect, actual) {
+  // If type of expect and actual is the same
+  if (typeof expect === typeof actual) {
+    // If type of both expect and actual is Array
+    if (typeof expect === "object") {
+      // If the length of both array expect and array actual not the same
+      if (expect.length !== actual.length) {
+        console.log(
+          `Expected array length ${expect.length} but found ${actual.length}`
+        );
+        throw new Error(
+          `Expected array length ${expect.length} but found ${actual.length}`
+        );
+      }
+      // If the length of both array expect and array actual the same
+      else {
+        // If the elements in expect are the same as elements in actual
+        if (
+          expect.every((element, index) => {
+            return element === actual[index];
+          })
+        ) {
+          console.log("_No Error_");
+        }
+        // If the elements in expect are not the same as elements in actual
+        else {
+          expect.forEach((element, index) => {
+            if (element !== actual[index]) {
+              console.log(`Expected "${element}" but found "${actual[index]}"`);
+              throw new Error(
+                `Expected  "${element}" but found "${actual[index]}"`
+              );
+            }
+          });
+        }
+      }
+    }
+    // If type of both expect and actual is not Array and expect and actual are not the same
+    else {
+      console.log(`Expected ${expect} but found ${actual}`);
+      throw new Error(`Expected ${expect} but found ${actual}`);
+    }
+  }
+  // If type of expect and actual are not the same
+  else {
+    console.log(
+      `Expected type ${typeof expect} but found type ${typeof actual}`
+    );
+    throw new Error(
+      `Expected type ${typeof expect} but found type ${typeof actual}`
+    );
+  }
+}
+
+module.exports = { assertEquals, assertNotEquals };
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,55 @@
-const assertEquals = require('./assert-equals')
+const { assertEquals, assertNotEquals } = require("./assert-equals");
 
-describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
-  })
+describe("assertEquals", () => {
+  describe("when expected and actual are the same string", () => {
+    it("returns without throwing an error", () => {
+      expect(() => assertEquals("abc", "abc")).not.toThrow();
+    });
+  });
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
-  })
-})
+  describe("when expected and actual are the same number", () => {
+    it("returns without throwing an error", () => {
+      expect(() => assertEquals(1, 1)).not.toThrow();
+    });
+  });
+
+  describe("when expected and actual are the same array", () => {
+    it("returns without throwing an error", () => {
+      expect(() =>
+        assertNotEquals(["a", "b", "c"], ["a", "b", "c"])
+      ).not.toThrow();
+    });
+  });
+});
+
+describe("assertNotEquals", () => {
+  describe("when expected and actual are different strings", () => {
+    it("returns with throwing an error", () => {
+      expect(() => assertNotEquals("abcef", "abc")).toThrow();
+    });
+  });
+
+  describe("when expected and actual are different numbers", () => {
+    it("returns with throwing an error", () => {
+      expect(() => assertNotEquals(1, 2)).toThrow();
+    });
+  });
+
+  describe("when expected and actual are different array same length", () => {
+    it("returns with throwing an error", () => {
+      expect(() => assertNotEquals(["a", "b"], ["a", "d"])).toThrow();
+    });
+  });
+
+  describe("when expected and actual are different arrays different lengths", () => {
+    it("returns with throwing an error", () => {
+      expect(() => assertNotEquals(["a", "b"], ["a", "b", "c"])).toThrow();
+    });
+  });
+
+  describe("when expected and actual are different types", () => {
+    it("returns with throwing an error", () => {
+      expect(() => assertNotEquals(1, "1")).toThrow();
+    });
+  });
+});
